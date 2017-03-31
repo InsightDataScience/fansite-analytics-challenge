abc7780921dde9866ae94e759deafd9c6e4bc1d6
David E Drummond <ddrum001@users.noreply.github.com>
2017-03-31 00:44:11 -0700

    Make language agnosticity clear in example src
    Explicitly state that the challenge is language agnostic.

--- src/process_log.py
+++ src/process_log.py
@@ -1,1 +1,2 @@
-// your python code to implement the features could be placed here
+// your Python code to implement the features could be placed here
+// note that you may use any language, there is no preference towards Python
